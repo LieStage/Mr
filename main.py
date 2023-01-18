c194692cbf26fadb6909a8134426e93a257c49f6
LieStage <118051769+LieStage@users.noreply.github.com>
2023-01-18 08:04:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,19 @@ from mdisk import iswin
 def echo(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
 
     if not checkuser(message):
-        app.send_message(message.chat.id, '__You are either not **Authorized** or **Banned**__', reply_to_message_id=message.id,reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("📦 Source Code", url="https://t.me/movie_timebot_only")]]))
+        app.send_message(message.chat.id,"""𝐻𝐸𝐿𝐿𝑂 👋
+
+𝑇𝐻𝐼𝑆 𝐴𝑅𝐸 𝑀𝑌 𝑃𝐿𝐴𝑁𝑆
+
+₹80/1$ - 1 𝑤𝑒𝑒𝑘
+
+₹120/2$ - 2 𝑤𝑒𝑒𝑘𝑠
+
+₹140/3$ - 3 𝑤𝑒𝑒𝑘𝑠
+
+₹160/4$ - 4 𝑤𝑒𝑒𝑘𝑠
+
+𝑈 𝑊𝐴𝑁𝑇 𝐽𝑈𝑆𝑇 𝐷𝑀 𝑀𝐸 :@fligher""", reply_to_message_id=message.id,reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("📦 Source Code", url="https://t.me/movie_time_botonly")]]))
         return
 
     app.send_message(message.chat.id, '**Hi, I am Mdisk Video Downloader, you can watch Videos without MX Player.\n__Send me a link to Start...__**',reply_to_message_id=message.id,
